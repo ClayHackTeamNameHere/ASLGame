1247c98abbb4f59614880ccbe9812dd0d31e55a3
Johnny9164 <96505092+Johnny9164@users.noreply.github.com>
2023-09-16 13:23:52 -0400

    fhdasui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,112 +2,80 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div style={{width: 360, height: 800, position: 'relative', background: 'white'}}>
+  <div style={{width: 360, height: 400, left: 0, top: 400, position: 'absolute'}}>
+    <div style={{width: 360, height: 400, left: 0, top: 0, position: 'absolute', background: '#3DB2FF', borderTopLeftRadius: 16, borderTopRightRadius: 16}} />
+    <div style={{width: 340, height: 160, left: 11, top: 72, position: 'absolute'}}>
+      <div style={{width: 340, height: 87, left: 0, top: 0, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 24, fontFamily: 'Poppins', fontWeight: '700', wordWrap: 'break-word'}}>Learn ASL</div>
+      <div style={{width: 266, height: 80, left: 36, top: 80, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 16, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>Learn Sign Language in real time though courses and fun games!  </div>
+    </div>
+    <div style={{width: 134, height: 56, left: 30, top: 302, position: 'absolute'}}>
+      <div style={{width: 134, height: 56, left: 0, top: 0, position: 'absolute', background: '#3DB2FF', borderRadius: 26.50, border: '2px #FFEDDA solid'}} />
+      <div style={{width: 5, height: 11, left: 105, top: 23, position: 'absolute', border: '2px #FFEDDA solid'}}></div>
+      <div style={{left: 23, top: 15, position: 'absolute', textAlign: 'center', color: '#FFEDDA', fontSize: 17, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>Register</div>
+    </div>
+    <div style={{width: 134, height: 56, left: 197, top: 302, position: 'absolute'}}>
+      <div style={{width: 134, height: 56, left: 0, top: 0, position: 'absolute', background: '#3DB2FF', borderRadius: 26.50, border: '2px #FFEDDA solid'}} />
+      <div style={{width: 134, height: 56, left: 0, top: 0, position: 'absolute', background: 'white', borderRadius: 26.50}} />
+      <div style={{width: 5, height: 11, left: 95, top: 23, position: 'absolute', border: '2px #3DB2FF solid'}}></div>
+      <div style={{left: 38, top: 15, position: 'absolute', textAlign: 'center', color: '#3DB2FF', fontSize: 17, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>Login</div>
+    </div>
+    <div style={{width: 69, height: 10, left: 146, top: 240, position: 'absolute'}}>
+      <div style={{width: 29, height: 10, left: 0, top: 0, position: 'absolute', background: '#FFEDDA', borderRadius: 5}} />
+      <div style={{width: 10, height: 10, left: 39, top: 0, position: 'absolute', background: 'rgba(255, 237, 218, 0.70)', borderRadius: 5}} />
+      <div style={{width: 10, height: 10, left: 59, top: 0, position: 'absolute', background: 'rgba(255, 237, 218, 0.70)', borderRadius: 5}} />
+    </div>
+    <div style={{width: 193, height: 34, left: 84, top: 1, position: 'absolute'}}>
+      <div style={{width: 193, height: 34, left: 0, top: 0, position: 'absolute'}} />
+      <div style={{width: 70, height: 4, left: 61, top: 17, position: 'absolute', background: 'rgba(255, 237, 218, 0.80)', borderRadius: 100}} />
+    </div>
+  </div>
+  <div style={{width: 444, height: 313, left: -42, top: 15, position: 'absolute'}}>
+    <div style={{width: 257.40, height: 253.25, left: 93.29, top: 59.75, position: 'absolute'}}>
+      <div style={{width: 187.93, height: 198.22, left: 39.90, top: 0, position: 'absolute', border: '2px white solid'}}></div>
+      <div style={{width: 245.85, height: 222.68, left: 0, top: 12.87, position: 'absolute'}}>
+        <div style={{width: 155.90, height: 164.33, left: 55.60, top: 3.95, position: 'absolute', background: '#263238'}}></div>
+        <div style={{padding: 10, left: -9.99, top: 134.06, position: 'absolute', borderRadius: 5}} />
+        <div style={{width: 78.09, height: 81.46, left: 92.27, top: 48.41, position: 'absolute', background: 'white'}}></div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div style={{width: 245.81, height: 37.01, left: 11.58, top: 216.24, position: 'absolute', background: '#263238'}}></div>
+    </div>
+    <div style={{width: 444, height: 296, left: 0, top: 0, position: 'absolute'}}>
+      <div style={{width: 444, height: 296, left: 0, top: 0, position: 'absolute'}}></div>
+      <div style={{width: 29.78, height: 30.16, left: 99.43, top: 211.02, position: 'absolute', background: 'linear-gradient(327deg, white 0%, #EEEEEE 100%)'}}></div>
+      <div style={{width: 30.73, height: 21.91, left: 295.06, top: 42.15, position: 'absolute', background: 'linear-gradient(48deg, white 0%, #EEEEEE 100%)'}}></div>
+      <div style={{width: 32.06, height: 32.46, left: 134.98, top: 51.73, position: 'absolute', background: 'linear-gradient(33deg, white 0%, #EEEEEE 100%)'}}></div>
+      <div style={{width: 14.97, height: 15.72, left: 105.03, top: 176.56, position: 'absolute', background: '#E1E4E5'}}></div>
+      <div style={{width: 8.39, height: 8.39, left: 231.87, top: 255.55, position: 'absolute', background: '#3DB2FF'}}></div>
+      <div style={{width: 17.28, height: 17.28, left: 333.49, top: 216.10, position: 'absolute', background: '#3DB2FF'}}></div>
+      <div style={{width: 13.99, height: 13.99, left: 342.37, top: 124.32, position: 'absolute', background: '#31446C'}}></div>
+      <div style={{width: 11.97, height: 11.97, left: 116.16, top: 105.32, position: 'absolute', background: '#F8AE9D'}}></div>
+      <div style={{width: 7.23, height: 6.78, left: 152.44, top: 248.64, position: 'absolute', background: '#E1E4E5'}}></div>
+      <div style={{width: 9.67, height: 9.26, left: 323.13, top: 178.59, position: 'absolute', background: '#E1E4E5'}}></div>
+      <div style={{width: 11.23, height: 11.23, left: 302.91, top: 230.88, position: 'absolute', background: '#E1E4E5'}}></div>
+      <div style={{width: 5.24, height: 5.99, left: 195.22, top: 53.44, position: 'absolute', background: '#E1E4E5'}}></div>
+      <div style={{width: 5.51, height: 5.51, left: 329.88, top: 146.46, position: 'absolute', transform: 'rotate(105deg)', transformOrigin: '0 0', background: '#E1E4E5'}}></div>
+      <div style={{width: 7.48, height: 7.48, left: 319.28, top: 107.79, position: 'absolute', background: '#E1E4E5'}}></div>
+      <div style={{width: 5.24, height: 4.49, left: 122.77, top: 138.33, position: 'absolute', background: '#E1E4E5'}}></div>
+      <div style={{width: 11.46, height: 11.46, left: 264.22, top: 48.38, position: 'absolute', transform: 'rotate(105deg)', transformOrigin: '0 0', background: '#E1E4E5'}}></div>
+      <div style={{width: 276.09, height: 91.67, left: 74.99, top: 73.80, position: 'absolute', background: '#E1E4E5'}}></div>
+    </div>
+  </div>
+  <div style={{width: 382, height: 34, left: -22, top: 0, position: 'absolute'}}>
+    <div style={{width: 382, height: 34, left: 0, top: 0, position: 'absolute'}} />
+    <div style={{width: 24.50, height: 10.50, left: 343, top: 13, position: 'absolute'}}>
+      <img style={{width: 22, height: 10.50, left: 0, top: 0, position: 'absolute', opacity: 0.35}} src="https://via.placeholder.com/22x10" />
+      <img style={{width: 1.50, height: 3.87, left: 23, top: 3.50, position: 'absolute', opacity: 0.35}} src="https://via.placeholder.com/1x4" />
+      <div style={{width: 18, height: 6.50, left: 2, top: 2, position: 'absolute', background: 'black', borderRadius: 1}} />
+    </div>
+    <img style={{width: 15.56, height: 8.47, left: 321.59, top: 13.39, position: 'absolute'}} src="https://via.placeholder.com/16x8" />
+    <img style={{width: 17.32, height: 8.24, left: 299.15, top: 13.65, position: 'absolute'}} src="https://via.placeholder.com/17x8" />
+    <div style={{width: 55.01, paddingTop: 5.41, left: 21.39, top: 5.41, position: 'absolute', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+      <div style={{flex: '1 1 0', alignSelf: 'stretch', paddingRight: 1.01, justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+        <div style={{width: 54, textAlign: 'center', color: '#1C1C1E', fontSize: 15, fontFamily: 'Inter', fontWeight: '500', lineHeight: 20, wordWrap: 'break-word'}}>9:41</div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
+  </div>
+</div>
   )
 }
